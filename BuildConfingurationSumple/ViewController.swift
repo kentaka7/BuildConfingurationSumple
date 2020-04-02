6126a1ca319b53d79b5fbe6e7cab017ac801bef6
kentaka7 <kentaka7777@gmail.com>
2020-04-02 13:51:14 +0900

    Initial Commit

---
+++ BuildConfingurationSumple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuildConfingurationSumple
+//
+//  Created by Naohiro Takakura on 2020/04/02.
+//  Copyright © 2020 Naohiro Takakura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
